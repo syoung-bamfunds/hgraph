0e02d6ff8cf485099b54923de3dd2feda2c115ca
Alexander Balabin <abalabin@bamfunds.com>
2024-09-18 13:57:26 +0100

    expose logger injection
    Signed-off-by: Alexander Balabin <abalabin@bamfunds.com>

--- src/hgraph/_runtime/_graph_runner.py
+++ src/hgraph/_runtime/_graph_runner.py
@@ -151,6 +151,7 @@ def run_graph(
     __trace__: bool | dict = False,
     __profile__: bool | dict = False,
     __trace_wiring__: bool | dict = False,
+    __logger__: Logger = None,
     **kwargs,
 ):
     """
@@ -170,7 +171,13 @@ def run_graph(
     :param life_cycle_observers: A list of observers to register with the runtime engine prior to evaluation.
     :param kwargs: Any additional kwargs to pass to the graph.
     """
-    kwargs_ = {"run_mode": run_mode, "trace": __trace__, "profile": __profile__, "trace_wiring": __trace_wiring__}
+    kwargs_ = {"run_mode": run_mode,
+               "trace": __trace__,
+               "profile": __profile__,
+               "trace_wiring": __trace_wiring__,
+               "graph_logger": __logger__
+               }
+
     if start_time is not None:
         kwargs_["start_time"] = start_time
     if end_time is not None:
